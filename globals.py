9d16a0dd59d78e97f71b8ef910c0fa46a33b8f65
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2012-09-26 11:13:05 +0200

    Added rudimentary read-write detection.
    If a line matches the regex
       <variable>\s*=[^=]
    
    then the line is interpreted as a write.

--- globals.py
+++ globals.py
@@ -1,4 +1,5 @@
 import clang.cindex
+import re
 from clang.cindex import CursorKind
 
 import collections
@@ -20,25 +21,28 @@ def recursor(node, indent=0):
         recursor(child, indent+1)
 
 def find_usage(node, glob, parents=None):
+    matcher = re.compile(glob + r"\s*=[^=]")
     if parents == None:
 	parents = []
     if node.displayname == glob and \
        node.kind != CursorKind.VAR_DECL and \
        node.kind == CursorKind.DECL_REF_EXPR:
-	print " > ", str(node.location.file) + ":" + ("%5i" % node.location.line) + ",",# filecache[node.location.file.name][node.location.line],
-	print "in %s:" % (",".join([p.displayname for p in parents if p.kind == CursorKind.FUNCTION_DECL])),
-	print filecache[node.location.file.name][node.location.line].strip()
-#	if parent:
-#	  print " >> ", str(parent.kind).split(".")[1]
+        line = filecache[node.location.file.name][node.location.line].strip()
+        yield {'fileline': line,
+                'write': bool(re.findall(glob + r"\s*=[^=]", line)),
+                'file': str(node.location.file),
+                'line#': node.location.line,
+                'infunction': ",".join([p.displayname for p in parents if p.kind == CursorKind.FUNCTION_DECL])}
 
     for child in node.get_children():
-	find_usage(child, glob, parents + [node])
+        for entry in find_usage(child, glob, parents + [node]):
+            yield entry
 
 def get_globals(parent, file):
     globals = {}
     for node in parent.get_children():
 	if node.kind == CursorKind.VAR_DECL and \
-	   node.location.file.name == file:
+           node.location.file.name == file:
             globals[node.displayname] = node
     return globals
 
@@ -46,11 +50,20 @@ def main(file):
     index = clang.cindex.Index.create()
     tu = index.parse(file)
     globals = get_globals(tu.cursor, file)
-    
+
     globkeys = sorted(globals.keys(), key=lambda x: x.lower())
     for glob in globkeys:
 	print glob, "at", globals[glob].location
-        find_usage(tu.cursor, glob)
+        results = [res for res in find_usage(tu.cursor, glob)]
+        results.sort(key=lambda x: (not x["write"], x["file"], x["line#"]))
+
+        fmt = "in %(file)s, on line %(line#) 6i, in %(infunction)s: %(fileline)s"
+
+        print "\n".join(
+            "> " + (fmt % res) for res in results if res["write"])
+        print ""
+        print "\n".join (
+            "  " + (fmt % res) for res in results if not res["write"])
 	print "-" * 40
 	print ""
 
